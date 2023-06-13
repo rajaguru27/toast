35bb1889b71d296ad5e1242f848c33378aa6a74d
rajaguru27 <jprsgurujprsguru@gmail.com>
2023-06-13 12:12:22 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+var view1=document.getElementById("view1");
+
+
+function viewDetails(){
+    view1.classList.add("view");
+}
+
+
+
+function viewDetail(){
+    view1.classList.remove("view");
+}
+
+var view2=document.getElementById("view2");
+
+function viewpara(){
+    view2.classList.add("view");
+}
+
+function viewparas(){
+    view2.classList.remove("view");
+}
